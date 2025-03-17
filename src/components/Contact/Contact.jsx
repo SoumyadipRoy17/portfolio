0361f5d2ae8088f64811972d624f4a54d8c3815c
Soumyadip Roy <soumyaroy172003@gmail.com>
2025-03-17 22:39:01 +0530

    Final commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:soumyaroy172003@gmail.com">
+            soumyaroy172003@gmail.com
+          </a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/soumyadip-roy-8994a3267/">
+            linkedin.com/Soumyadip Roy
+          </a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https:https://github.com/SoumyadipRoy17">
+            github.com/SoumyadipRoy17
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
